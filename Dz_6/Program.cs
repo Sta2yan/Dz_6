b01168ade2329d409c5b1fc0fbd30b7087d5be4b
Sta2yan <s2_stas@mail.ru>
2022-02-26 20:36:09 +0300

    Добавьте файлы проекта.

---
+++ Dz_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userAge;
+            string userWork;
+
+            Console.Write("Как вас зовут? ");
+            userName = Console.ReadLine();
+            Console.Write("Сколько вам лет? ");
+            userAge = Console.ReadLine();
+            Console.Write("Кем вы работаете? ");
+            userWork = Console.ReadLine();
+
+            Console.WriteLine($"\nВас зовут {userName}" +
+                              $"\nВам {userAge} года" +
+                              $"\nВы работаете {userWork}");
+        }
+    }
+}
